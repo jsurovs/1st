788c9028baa6ca9e68dc1996b961f0adca707c40
danielsjirgensons <60740580+danielsjirgensons@users.noreply.github.com>
2021-02-08 20:26:04 +0200

    Improved and fixed app

--- assets/js/hide-result-block.js
+++ assets/js/hide-result-block.js
@@ -1,10 +1,23 @@
-function submit(){
+// Validating gross salary
+function submitInput(){
+	var grossSalary = document.querySelector(".grossInput");
+
+	if(grossSalary.value <= 0){
+		alert("Ievadiet bruto algu!");
+		grossSalary.focus();
+	}else{
+		submit(grossSalary.value);
+	}
+}
+
+function submit(gross){
 	
 	//Set visible result section
 	const newLocal = "visible"; document.getElementById("resultsBlock").style.visibility=newLocal;
 	
 	//Variables
-	var grossSalary = document.querySelector(".grossInput").value;
+	// var grossSalary = document.querySelector(".grossInput").value;
+	var grossSalary = gross;
 	var socialEmployeeRate = document.getElementById("socErate").value;
 	var incomeTaxRate = document.querySelector(".incomeRate").value;
 	var Dependents = document.querySelector(".dependents").value;
@@ -25,7 +38,9 @@ function submit(){
 	//Personal Income Tax
 	var calcIINsubtotal = parseFloat(grossSalary-calcSocE-calcDependents);
 	calcIIN = parseFloat(calcIINsubtotal*(incomeTaxRate / 100));
-	document.querySelector(".resultIncomeTax").innerHTML = calcIIN.toFixed(2) + " EUR";
+
+	var outputIIN = (calcIIN <= 0) ? "0.00 EUR" : calcIIN.toFixed(2) + " EUR";
+	document.querySelector(".resultIncomeTax").innerHTML = outputIIN;
 	
 	//Salary calculation
 	calcGrossResult = (grossSalary - calcSocE - calcIIN);
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,15 @@ function taxBookSubmitted(){
 	var highRate = "23.00";
 	var lowRate = "20.00";
 
-	if(status = "20"){
-		document.querySelector(".incomeRate").value = lowRate;
-		document.getElementById("neapl").style.visibility="visible";
-	} else if(status = "23"){
-		document.querySelector(".incomeRate").value = highRate;
-	}
+	// if(status == "20"){
+	// 	document.querySelector(".incomeRate").value = lowRate;
+	// 	document.getElementById("neapl").style.visibility="visible";
+	// } else if(status == "23"){
+	// 	document.querySelector(".incomeRate").value = highRate;
+	// 	document.getElementById("neapl").style.visibility="hidden";
+	// }
+
+	document.querySelector(".incomeRate").value = (status == 20) ? lowRate : highRate;
+	document.getElementById("neapl").style.visibility = (status == 20) ? "visible" : "hidden";
+
 };
